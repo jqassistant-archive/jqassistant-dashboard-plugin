8aca61f537cc8e1ace43f4ad0a2a510f9fcf3367
Sebastian Wendorf <sebastian.wendorf@buschmais.com>
2019-08-22 12:39:27 +0200

    Refine tests

--- src/test/java/org/jqassistant/contrib/plugin/dashboard/DashboardIT.java
+++ src/test/java/org/jqassistant/contrib/plugin/dashboard/DashboardIT.java
@@ -3,8 +3,13 @@ package org.jqassistant.contrib.plugin.dashboard;
 import com.buschmais.jqassistant.core.analysis.api.Result;
 import com.buschmais.jqassistant.core.analysis.api.rule.Concept;
 import com.buschmais.jqassistant.core.analysis.api.rule.RuleException;
+import com.buschmais.jqassistant.plugin.common.api.model.FileDescriptor;
+import com.buschmais.jqassistant.plugin.java.api.model.PackageDescriptor;
+import com.buschmais.jqassistant.plugin.java.api.model.TypeDescriptor;
 import com.buschmais.jqassistant.plugin.java.test.AbstractJavaPluginIT;
+import de.kontext_e.jqassistant.plugin.git.store.descriptor.*;
 import org.junit.Test;
+
 import java.util.Map;
 
 import static com.buschmais.jqassistant.core.analysis.api.Result.Status.FAILURE;
@@ -20,7 +25,30 @@ public class DashboardIT extends AbstractJavaPluginIT {
      */
     @Test
     public void validAuthors() throws RuleException {
-        assertThat(super.applyConcept("dashboard:Authors").getStatus(), equalTo(FAILURE));
+        store.beginTransaction();
+        GitAuthorDescriptor author1 = store.create(GitAuthorDescriptor.class);
+        author1.setName("Bob");
+        author1.setEmail("bob1@example.com");
+        author1.setIdentString("1");
+        GitAuthorDescriptor author2 = store.create(GitAuthorDescriptor.class);
+        author2.setName("Bob");
+        author2.setEmail("bob2@example.com");
+        author2.setIdentString("2");
+        GitCommitDescriptor commit1 = store.create(GitCommitDescriptor.class);
+        commit1.setAuthor("1");
+        commit1.setCommitter("1");
+        author1.getCommits().add(commit1);
+        GitCommitDescriptor commit2 = store.create(GitCommitDescriptor.class);
+        commit2.setAuthor("2");
+        commit2.setCommitter("2");
+        author2.getCommits().add(commit2);
+        store.commitTransaction();
+
+        Result<Concept> result = applyConcept("dashboard:Authors");
+        assertThat(result.getStatus(), equalTo(SUCCESS));
+        assertThat(result.getRows().size(), equalTo(1));
+
+        store.reset();
     }
 
     /**
@@ -28,7 +56,7 @@ public class DashboardIT extends AbstractJavaPluginIT {
      */
     @Test
     public void validMerge() throws RuleException {
-        Result<Concept> result = super.applyConcept("dashboard:Merge");
+        Result<Concept> result = applyConcept("dashboard:Merge");
         assertThat(result.getStatus(), equalTo(SUCCESS));
         assertThat(result.getRows().size(), equalTo(1));
         assertThat(result.getRows().get(0).containsKey("count(c)"), equalTo(true));
@@ -40,7 +68,19 @@ public class DashboardIT extends AbstractJavaPluginIT {
      */
     @Test
     public void validTimetree() throws RuleException {
-        assertThat(super.applyConcept("dashboard:Timetree").getStatus(), equalTo(FAILURE));
+        store.beginTransaction();
+        GitCommitDescriptor commit = store.create(GitCommitDescriptor.class);
+        commit.setDate("2077-10-22");
+        store.commitTransaction();
+
+        Result<Concept> result = applyConcept("dashboard:Timetree");
+        assertThat(result.getStatus(), equalTo(SUCCESS));
+        assertThat(result.getRows().size(), equalTo(1));
+        assertThat(result.getRows().get(0).get("y"), notNullValue());
+        assertThat(result.getRows().get(0).get("m"), notNullValue());
+        assertThat(result.getRows().get(0).get("d"), notNullValue());
+
+        store.reset();
     }
 
     /**
@@ -48,7 +88,19 @@ public class DashboardIT extends AbstractJavaPluginIT {
      */
     @Test
     public void validGitFileName() throws RuleException {
-        assertThat(super.applyConcept("dashboard:GitFileName").getStatus(), equalTo(FAILURE));
+        store.beginTransaction();
+        GitFileDescriptor file1 = store.create(GitFileDescriptor.class);
+        file1.setRelativePath("file1.txt");
+        GitFileDescriptor file2 = store.create(GitFileDescriptor.class);
+        file2.setRelativePath("file2.config");
+        store.commitTransaction();
+
+        Result<Concept> result = applyConcept("dashboard:GitFileName");
+        assertThat(result.getStatus(), equalTo(SUCCESS));
+        assertThat(result.getRows().size(), equalTo(1));
+        assertThat((Long)result.getRows().get(0).get("count(f)"), equalTo(2L));
+
+        store.reset();
     }
 
     /**
@@ -56,7 +108,22 @@ public class DashboardIT extends AbstractJavaPluginIT {
      */
     @Test
     public void validTypeHasSourceGitFile() throws RuleException {
-        assertThat(super.applyConcept("dashboard:TypeHasSourceGitFile").getStatus(), equalTo(FAILURE));
+//        store.beginTransaction();
+//        FileDescriptor file = store.create(FileDescriptor.class);
+//        file.setFileName("Test.java");
+//        PackageDescriptor pack = store.create(PackageDescriptor.class);
+//        pack.setFileName("/package");
+//        pack.getContains().add(file);
+//        GitFileDescriptor gitFile = store.create(GitFileDescriptor.class);
+//        gitFile.setRelativePath("./package/Test.java");
+//        store.commitTransaction();
+
+
+        Result<Concept> result = applyConcept("dashboard:TypeHasSourceGitFile");
+        assertThat(result.getStatus(), equalTo(FAILURE));
+//        assertThat(result.getStatus(), equalTo(SUCCESS));
+
+//        store.reset();
     }
 
     /**
@@ -64,7 +131,17 @@ public class DashboardIT extends AbstractJavaPluginIT {
      */
     @Test
     public void validFiletype() throws RuleException {
-        assertThat(super.applyConcept("dashboard:Filetype").getStatus(), equalTo(FAILURE));
+        store.beginTransaction();
+        GitFileDescriptor file1 = store.create(GitFileDescriptor.class);
+        file1.setRelativePath("file1.txt");
+        store.commitTransaction();
+
+        Result<Concept> result = applyConcept("dashboard:Filetype");
+        assertThat(result.getStatus(), equalTo(SUCCESS));
+        assertThat(result.getRows().size(), equalTo(1));
+        assertThat((String)result.getRows().get(0).get("filetype"), equalTo("txt"));
+
+        store.reset();
     }
 
     /**
